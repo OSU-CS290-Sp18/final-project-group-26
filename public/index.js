505a36a81cdcd2214ff3501ca9aec63ab21c102d
Michael Wang <wangshiyao96@gmail.com>
2018-06-07 00:29:04 -0700

    Update the server code and template structure
    1. Update test

---
+++ public/index.js
@@ -0,0 +1,16 @@
+/*
+function jumptoPage(target) {
+    xhttp.send('/' + target);
+}
+
+
+
+/*****This is the listener function*****
+
+var recommend_button = document.getElementById('nav_recommend');
+if (recommend_button) {
+    //This is used to just to the recommendation which is the index page
+    recommend_button.addEventListener('click', jumptoPage('indexpage'));
+}
+
+*/
