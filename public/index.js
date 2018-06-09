3e854f0896caea46e1abcd2773901a5e78e5203b
HaoChong Pan <32114924+Joeking123@users.noreply.github.com>
2018-06-09 12:27:35 -0700

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -34,15 +34,30 @@ if(createButton){
    createButton.addEventListener('click', showCreateRecipeModal);
 }
 
+//this will be the function to clean all input data after user click cancel
+// button, close button or accept button
+function cleanInputData(){
+    var recipeInputElements = document.getElementsByClassName('recipe-input-element');
+
+    for(var i=0; i < recipeInputElements.length; i++){
+      var input = recipeInputElements[i].querySelector('input,textarea');
+      input.value = '';
+    }
+}
 
 function hideModal(){
-  var modalBackdrop = document.getElementsByClassName('hidden');
-  for(var i = 0 ; i < modalBackdrop.length; i++){
-        modalBackdrop[i].style.display = 'none';
-  }
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var CreateRecipeModal = document.getElementById('create-recipe-modal');
+
+  modalBackdrop.classList.add('hidden');
+  CreateRecipeModal.classList.add('hidden');
+
+  cleanInputData();
+
 }
 //This will be click hidden function
 
-var CancelBackdrop = document.querySelector('.modal-cancel-button');
-
-CancelBackdrop.addEventListener('click', hideModal);
+var CancelButton = document.querySelector('.modal-cancel-button');
+var CloseButton = document.querySelector('.modal-close-button')
+CancelButton  .addEventListener('click', hideModal);
+CloseButton.addEventListener('click', hideModal);
