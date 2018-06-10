ba81e3bc5d5ea82e798fa9fd6be74e983e68dad8
wangmuq <wangmuq@oregonstate.edu>
2018-06-10 00:36:36 -0700

    change test recipe to object

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var indexData = require('./indexData');
+var meatData = require('./meatData');
+var seafData = require('./seafData');
+var vegData = require('./vegData');
+var dessData = require('./dessData');
+var soupData = require('./soupData');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -13,14 +19,16 @@ app.set('view engine', 'handlebars');
 /*****This is the test object*****/
 //The structure of the test object should be changed, this is currently used for
 //alpha version of the web app
-var recipe_obj = {
+
+
+/*var recipe_obj = {
     name: 'Stir - Fried Shrimp and Scallions',
     photoURL: 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F200911-xl-mama-changs-stir-fried-shrimp-and-scallions.jpg%3Fitok%3DdORctdLA&w=800&q=85',
     ingredient: "1/2 pounds shelled and deveined large shrimp; 3 garlic clovesSliced One; 1-inch piece of fresh ginger",
     direction: "Step 1: In a large bowl, toss the shrimp with the garlic, ginger, red pepper, egg white and 1 teaspoon of the cornstarch until well-coated; Step 2: In a medium bowl, whisk the ketchup with the broth, sugar, pepper, salt and the remaining 1 teaspoon of cornstarch; Step 3: In a very large skillet, heat the oil until shimmering. Add the shrimp and stir-fry over high heat until they begin to turn pink. Add the ketchup mixture and simmer until the shrimp are cooked, about 2 minutes. Stir in the scallions and cilantro and serve.",
     href: 'none'
 };
-
+*/
 var category_list =[
     {
         name: 'Meat',
@@ -50,7 +58,7 @@ var category_list =[
 
 ]
 
-
+/*
 var meat_list = [
     recipe_obj,
     recipe_obj,
@@ -94,78 +102,96 @@ var recipe_list = [
     recipe_obj,
     recipe_obj,
     recipe_obj
-];
+];*/
 
 
-//Use to respond to the request of the index/recommend page
 
-app.get('/', function(req,res){
-		res.status(200).render('Index',{
-			recommands: recipe_list
-		});
+
+
+
+app.get('/',function(req,res,next){
+    if(indexData){
+        var templateArgs = {
+            recommands: indexData
+        }
+        res.status(200).render('Index',templateArgs);
+    }
+    else{
+        next();
+    }
 });
 
-
-app.use(express.static('public'));
-
-
-//Use to respond to the request of the category page
-
-app.get('/category', function (req, res,nect) {
+app.get('/category', function (req, res,next) {
     res.status(200).render('Category', {
         category_recipe: category_list
     });
 });
 
-//Use to respond to which category is requested
-
-app.get('/category/meat', function(req, res){
-    res.status(200).render('OneCategory', {
-        one_category: meat_list
-    });
+app.get('/category/meat', function(req, res, next){
+    if(meatData){
+        var templateArgs = {
+            one_category: meatData
+        }
+        res.status(200).render('OneCategory',templateArgs);
+    }
+    else{
+        next();
+    }
 });
 
-
-
-app.get('/category/seafood', function(req,res){
-    res.status(200).render('OneCategory',{
-        one_category: sea_food_list
-    });
-})
-app.get('/category/vegetables', function(req,res){
-    res.status(200).render('OneCategory',{
-        one_category: veg_list
-    });
-});
-app.get('/category/soup', function(req,res){
-    res.status(200).render('OneCategory',{
-        one_category: soup_list
-    });
-});
-app.get('/category/dessert', function(req,res){
-    res.status(200).render('OneCategory',{
-        one_category: dessert_list
-    });
-});
-app.get('/category/others', function(req,res){
-    res.status(200).render('OneCategory',{
-        one_category: others_list
-    });
+app.get('/category/seadfood', function(req, res, next){
+    if(seafData){
+        var templateArgs = {
+            one_category: seafData
+        }
+        res.status(200).render('OneCategory',templateArgs);
+    }
+    else{
+        next();
+    }
 });
 
-
-//Use to respond to the request of the single recipe page
-
-app.get('/recipes', function (req, res) {
-    res.status(200).render('AllRecipes', {
-        recipes: recipe_list
-    });
+app.get('/category/vegetables', function(req, res, next){
+    if(vegData){
+        var templateArgs = {
+            one_category: vegData
+        }
+        res.status(200).render('OneCategory',templateArgs);
+    }
+    else{
+        next();
+    }
 });
 
+app.get('/category/dessert', function(req, res, next){
+    if(dessData){
+        var templateArgs = {
+            one_category: dessData
+        }
+        res.status(200).render('OneCategory',templateArgs);
+    }
+    else{
+        next();
+    }
+});
 
+app.get('/category/soup', function(req, res, next){
+    if(soupData){
+        var templateArgs = {
+            one_category: soupData
+        }
+        res.status(200).render('OneCategory',templateArgs);
+    }
+    else{
+        next();
+    }
+});
+//Use to respond to the request of the index/recommend page
 
 
 //Use to show the 404 page
+
+app.use(express.static('public'));
 
 app.get('*', function(req,res){
 	res.status(404).render('404');
